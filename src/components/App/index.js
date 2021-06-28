0e78221ea609214755491ea5efbb307c75c7ac6a
Matías Raul Fito <matias.fito@outlook.com>
2021-06-28 18:40:35 -0300

    Setup Material-UI

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const App = () => {
+  return (
+    <div>
+      <Typography variant="h2">
+        Hello World
+      </Typography>
+      <Typography variant="subtitle1">
+        Welcome to OMDB
+      </Typography>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import App from './components/App';
 
-const HelloWorld = () => (
-  <h1 className="main-title">
-    Hello World asd 3
-  </h1>
-);
-
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
