830131d313012c81a10f6ad492cf0a641405bada
Matías Raul Fito <matias.fito@outlook.com>
2021-06-28 11:33:06 -0300

    Babel And React Setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-(function helloWorld() {
-  console.log('Hello World');
-}());
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const HelloWorld = () => {
+  return (
+    <h1 className="main-title">
+      Hello World
+    </h1>
+  );
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
