dc598160497850d42457ca380be1e476aa55ce92
Matías Raul Fito <matias.fito@outlook.com>
2021-06-28 13:11:02 -0300

    Setup ESLint

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-const HelloWorld = () => {
-  return (
-    <h1 className="main-title">
-      Hello World
-    </h1>
-  );
-}
+const HelloWorld = () => (
+  <h1 className="main-title">
+    Hello World asd 3
+  </h1>
+);
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "index.bundle.js"
+    path: path.join(__dirname, 'build'),
+    filename: 'index.bundle.js',
   },
-  mode: process.env.NODE_ENV || "development",
+  mode: process.env.NODE_ENV || 'development',
   resolve: {
     modules: [
-      path.resolve(__dirname, "src"),
-      "node_modules"
-    ]
+      path.resolve(__dirname, 'src'),
+      'node_modules',
+    ],
   },
   devServer: {
     port: 3000,
-    contentBase: path.join(__dirname, "src")
+    contentBase: path.join(__dirname, 'src'),
   },
   module: {
     rules: [
-      { 
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.(css|scss)$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
-      { 
+      {
         test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        use: ["file-loader"]
+        use: ['file-loader'],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html")
-    })
-  ]
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+    new ESLintWebpackPlugin({
+      fix: true,
+    }),
+  ],
 };
